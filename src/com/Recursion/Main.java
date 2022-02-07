a43d815d0d9bce7ab648909bebc5de9af716c1d1
Nitin <nsinghboss02@gmail.com>
2022-02-07 19:15:51 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,45 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+    private Node head;
+    private Node tail;
+    private int size;
+    public Main(){
+        this.size = 0;
+    }
+    public void insertFirst(int value){
+        Node node = new Node(value);
+        node.next = head;
+        head = node;
+        if(tail==null){
+            tail = head;
+        }
+        size = size+1;
+    }
+    /*
+    Taken a third variable temp which will be equal to head so till it becomes null display of content
+    will be continued.
+     */
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + "-->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    private class Node{
+        // value = value that is given in a node
+        // Node = it will point to the next node.
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+        public Node(int value,Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ src/com/Recursion/mainMethod.java
@@ -0,0 +1,12 @@
+package com.Recursion;
+
+public class mainMethod {
+    public static void main(String[] args) {
+        Main list = new Main();
+        list.insertFirst(2);
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(5);
+        list.display();
+    }
+}
